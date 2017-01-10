af70af9f615cf01980602fc834c07292c4de63b3
Christiano Matos <chmatos@chmatos.com>
2017-01-10 15:17:36 -0200

    add enum

--- models/default/model.rb
+++ models/default/model.rb
@@ -1,6 +1,7 @@
 class ##{table.camelize} < ActiveRecord::Base
   ##{belongs_to_list}
   ##{has_many_list}
+  ##{enum_list}
   def self.search(search)
     if search
       where("lower(##{search}) LIKE ?", "%#{search.downcase}%")
